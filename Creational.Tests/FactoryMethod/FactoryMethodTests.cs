b2cc3d0dea0789d4448cbabfd45865456725af1a
gsoulavy <g.soulavy@gmail.com>
2017-10-31 21:06:24 +0000

    [FactoryMethod]: Add factory method.

---
+++ Creational.Tests/FactoryMethod/FactoryMethodTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Creational.Tests.FactoryMethod
+{
+    using Creational.FactoryMethod;
+    using Xunit;
+
+    public class FactoryMethodTests
+    {
+        [Fact(DisplayName = "FactoryMethod: get default")]
+        public void GetDefaultCarFactoryTest()
+        {
+            var client = new Client();
+            var car = client.Run();
+            Assert.Equal("Porsche", car.Name);
+        }
+
+        [Fact(DisplayName = "FactoryMethod: get selected")]
+        public void GetTrabantFactoryTest()
+        {
+            var client = new Client();
+            var car = client.Run("Creational.FactoryMethod.TrabantFactory");
+            Assert.Equal("Trabant", car.Name);
+        }
+    }
+}
---
+++ Creational/FactoryMethod/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace Creational.FactoryMethod
+{
+    using System;
+    using System.Reflection;
+
+    public class Client
+    {
+        public ICar Run(string factoryName = "Creational.FactoryMethod.PorscheFactory")
+        {
+            var factory = Assembly.GetExecutingAssembly().CreateInstance(factoryName) as ICarFactory;
+            return factory?.Create();
+        }
+    }
+}
