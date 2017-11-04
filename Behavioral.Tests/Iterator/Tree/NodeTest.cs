380bb149c28b5157be178969217594396eadcdc8
gsoulavy <g.soulavy@gmail.com>
2017-11-04 16:27:40 +0000

    [Iterator]: Add iterator to the solution

---
+++ Behavioral.Tests/Iterator/Tree/NodeTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Behavioral.Tests.Iterator.Tree
+{
+    using System.Collections.Generic;
+    using Xunit;
+    using Behavioral.Iterator.Tree;
+
+    public class NodeTest
+    {
+        [Fact(DisplayName = "Iterator: Tree")]
+        public void IteratorTest()
+        {
+            var node = new Node<int>(5);
+            node.Add(3);
+            node.Add(1);
+            node.Add(4);
+            node.Add(9);
+            node.Add(11);
+            Assert.NotNull(node);
+
+            var values = new List<int>();
+            foreach (var i in node)
+            {
+                values.Add(i);
+            }
+
+            var result = string.Join(" ", values);
+
+            Assert.Equal("5 3 4 9 1 11", result);
+        }
+    }
+}
---
+++ Behavioral/Iterator/Tree/Node.cs
@@ -0,0 +1,141 @@
+﻿namespace Behavioral.Iterator.Tree
+{
+    using static System.Math;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    [DebuggerDisplay("Value: {Value}, Left: {HasLeft}, Right: {HasRight}")]
+    public class Node<T> : IEnumerable<T>
+    {
+        public T Value { get; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public bool HasLeft => Left != null;
+
+        public bool HasRight => Right != null;
+
+        public bool IsLeftPriority => Left?.Depth() <= Right?.Depth();
+
+        public void Add(T value)
+        {
+            if (!HasLeft)
+            {
+                Left = new Node<T>(value);
+                return;
+            }
+            if (!HasRight)
+            {
+                Right = new Node<T>(value);
+                return;
+            }
+            if (IsLeftPriority)
+                Left.Add(value);
+            else
+                Right.Add(value);
+        }
+
+        public int Depth()
+        {
+            if (HasLeft && HasRight)
+            {
+                var max = Max((int)Left?.Depth(), (int)Right?.Depth());
+                return ++max;
+            }
+            return 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new NodeEnumerator<T>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class NodeEnumerator<T> : IEnumerator<T>
+    {
+        private readonly Node<T> _nodes;
+        private Node<T> _current;
+        private Node<T> _previousStack;
+        private Stack<Node<T>> _breadCrumb = new Stack<Node<T>>();
+
+        public NodeEnumerator(Node<T> nodes)
+        {
+            _nodes = nodes;
+        }
+        public bool MoveNext()
+        {
+            if (_current == null)
+            {
+                Reset();
+                _current = _nodes;
+                return true;
+            }
+            if (_current.HasLeft)
+                return TraverseLeft();
+            if (_current.HasRight)
+                return TraverseRight();
+            return TraverseUpAndRight();
+        }
+
+        private bool TraverseLeft()
+        {
+            _breadCrumb.Push(_current);
+            _current = _current.Left;
+            return true;
+        }
+
+        private bool TraverseRight()
+        {
+            _breadCrumb.Push(_current);
+            _current = _current.Right;
+            return true;
+        }
+
+        private bool TraverseUpAndRight()
+        {
+            if (_breadCrumb.Any())
+            {
+                _previousStack = _current;
+                while (true)
+                {
+                    _current = _breadCrumb.Pop();
+                    if (_previousStack != _current.Right) break;
+                }
+                if (_current.HasRight)
+                {
+                    return TraverseRight();
+                }
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _current = null;
+        }
+
+        public T Current => _current.Value;
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
